0ded92b60d987f8177cfff32f084f076c60a6697
Timothy Lou <tmthlou@gmail.com>
2026-02-02 20:07:38 -0800

    testing centering div

--- syllabi-app/frontend/src/app/has_navbar/upload/page.tsx
+++ syllabi-app/frontend/src/app/has_navbar/upload/page.tsx
@@ -4,7 +4,7 @@ import Navbar from "../../components/navbar";
 export default function UploadPage() {
   return (
     <main className="min-h-screen flex items-center justify-center">
-      <div className="max-w-md w-full p-6">
+      <div className="max-w-md w-full p-6 justify-center">
         <h1 className="text-2xl font-semibold mb-4 text-center">
           Upload your PDF
         </h1>
